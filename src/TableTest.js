8983a25280e470947cbd046e1a9c8e676c6634fa
rpdict <rpdict@qq.com>
2018-03-05 13:24:40 +0800

    init version

---
+++ src/TableTest.js
@@ -0,0 +1,82 @@
+import { Table } from 'antd';
+import reqwest from 'reqwest';
+import * as React from 'react';
+import './App.css';
+
+const columns = [{
+  title: 'Id',
+  dataIndex: 'id',
+  sorter: true,
+}, {
+  title: 'Email',
+  dataIndex: 'email',
+}, {
+  title: 'Name',
+  dataIndex: 'name',
+}];
+
+class TableTest extends React.Component {
+    state = {
+      data: [],
+      pagination: {},
+      loading: false,
+    };
+
+    componentDidMount() {
+      this.fetch();
+    }
+
+    handleTableChange = (pagination, filters, sorter) => {
+      const pager = { ...this.state.pagination };
+      pager.current = pagination.current;
+      this.setState({
+        pagination: pager,
+      });
+      this.fetch({
+        results: pagination.pageSize,
+        page: pagination.current,
+        sortField: sorter.field,
+        sortOrder: sorter.order,
+        ...filters,
+      });
+    }
+
+    fetch = (params = {}) => {
+      console.log('params:', params);
+      this.setState({ loading: true });
+      reqwest({
+        url: 'http://10.2.4.18:8080/demo/all',
+        method: 'get',
+        data: {
+          results: 10,
+          ...params,
+        },
+        type: 'json',
+      }).then((data) => {
+        const pagination = { ...this.state.pagination };
+        // Read total count from server
+        pagination.total = data.results.totalElements;
+        // pagination.total = 200;
+        this.setState({
+          loading: false,
+          data: data.results.content,
+          pagination,
+        });
+      });
+    }
+
+    render() {
+      return (
+        <Table
+          columns={columns}
+          rowKey="id"
+          dataSource={this.state.data}
+          pagination={this.state.pagination}
+          loading={this.state.loading}
+          onChange={this.handleTableChange}
+        />
+      );
+    }
+}
+
+export default TableTest;
